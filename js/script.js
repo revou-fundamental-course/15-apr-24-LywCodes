e4ada6284e2ee77ecb81f872c7d479dbff306339
LywCodes <lim.lywen15@gmail.com>
2024-04-20 16:16:40 +0700

    lil up

--- js/script.js
+++ js/script.js
@@ -31,38 +31,6 @@ function reverse() {
 
 //* Fahrenheit to Celsius//
 
-// function fahrenheitToCelsius(fahrenheit) {
-//     return (fahrenheit - 32) * 5 / 9;
-// }
-
-
-// function convertFahrenheit() {
-//     let fahrenheitInput = parseFloat(document.getElementById('input-fahrenheit').value); 
-
-//     if (!isNaN(fahrenheitInput)) {
-//         let celsiusOutput = (fahrenheitInput - 32) * 5/9;
-//         let solution = '('+fahrenheitInput + '°F - 32) * (5 / 9) = ' + celsiusOutput.toFixed(2) + '°C';
-
-//         document.getElementById('result-fahrenheit').innerText = celsiusOutput.toFixed(2);
-//         document.getElementById('method-output').innerText = solution;
-//     } else {
-//         document.getElementById('result-fahrenheit').innerText = '';
-//         document.getElementById('method-output').innerText = '';
-//     }
-//     return false; 
-// }
-
-
-// function resetFahrenheit() {
-//     document.getElementById('input-fahrenheit').value = '';
-//     document.getElementById('result-fahrenheit').textContent = '';
-//     document.getElementById('method-outputs').textContent = '';
-// }
-
-
-// function reverseFahrenheit() {
-//     window.location.href = 'index.html';
-// }
 
 function convertFahrenheit() {
     let fahrenheitInput = parseFloat(document.getElementById('input-fahrenheit').value); 
